18e64846d0a334646cdcc9b80b3f29f22f3ec55c
B. Haki Martin <jojackblack@gmail.com>
2024-11-25 14:52:42 -0500

    put navigation in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Contact from "@/lib/components/Contact";
 import Gamedev from "@/lib/components/Gamedev";
+import Navigation from "@/lib/components/Navigation";
 import Webdev from "@/lib/components/Webdev";
 import Welcome from "@/lib/components/Welcome";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-1 grid-flow-row-dense items-center justify-items-center min-h-screen">
+    <div className="grid grid-flow-row-dense items-center justify-items-center min-h-screen">
+      <Navigation />
       <Welcome />
       <Webdev />
       <Gamedev />
