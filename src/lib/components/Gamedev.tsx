e9c82d8c7a08fb36dca41bca28154754ba92b182
B. Haki Martin <jojackblack@gmail.com>
2024-11-25 22:46:27 -0500

    created links to each different section

--- src/lib/components/Gamedev.tsx
+++ src/lib/components/Gamedev.tsx
@@ -4,6 +4,15 @@ function Gamedev() {
   return (
     <div id='gamedev'>
       My game development stuff
+      <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer efficitur quam nec luctus aliquet. Integer nec purus imperdiet nulla rutrum congue. Proin luctus, lacus in finibus convallis, erat ligula facilisis diam, et iaculis eros metus nec tortor. Aenean sollicitudin laoreet sapien. Curabitur tempor gravida mauris, quis commodo metus dapibus ut. Vestibulum eleifend nisl in sollicitudin faucibus. Donec vitae enim vitae erat vehicula laoreet eu sed nibh. Curabitur tempor urna vitae dui tincidunt suscipit. Pellentesque sodales sodales tempor. Fusce quis lacinia elit. Sed commodo at turpis non euismod. Etiam eleifend sapien nec nisl sollicitudin lobortis. Morbi vel semper diam.
+      </p>
+      <p>
+Maecenas dui turpis, viverra laoreet laoreet vel, maximus non odio. Mauris ut justo eget dui ullamcorper malesuada. Aliquam sit amet pharetra odio. Mauris ac ipsum non dolor vehicula molestie a et leo. Sed egestas eros eros. Aenean a ligula nulla. Nulla euismod, est at dapibus faucibus, nunc libero aliquet nisl, non rutrum justo lacus eu sapien. Nullam enim ligula, tincidunt luctus gravida quis, cursus efficitur diam. Phasellus mollis lorem eget sapien faucibus egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vestibulum purus vitae tortor tristique sollicitudin. Sed non tincidunt magna.
+      </p>
+      <p>
+Fusce consectetur tempor turpis eget pretium. Mauris at magna neque. Suspendisse sed neque sit amet felis imperdiet tristique non et odio. Fusce ut diam eget lacus luctus blandit. Nullam convallis vel urna in bibendum. Suspendisse fringilla tincidunt orci, ac viverra libero lacinia in. Mauris suscipit nibh eu euismod fermentum. Fusce nec feugiat est. Cras id enim posuere, ullamcorper eros id, placerat turpis. 
+      </p>
     </div>
   )
 }
--- src/lib/components/Navigation.tsx
+++ src/lib/components/Navigation.tsx
@@ -1,21 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Navigation() {
   return (
     <div>
       <ul>
-        <li>
-            Welcome
-        </li>
-        <li>
-            Webdev
-        </li>
-        <li>
-            Gamedev
-        </li>
-        <li>
-            Contact
-        </li>
+        <Link href={"#*"}>
+          <li>
+              Welcome
+          </li>
+        </Link>
+        <Link href={"#webdev"}>
+          <li>
+              Webdev
+          </li>
+        </Link>
+        <Link href={"#gamedev"}>
+          <li>
+              Gamedev
+          </li>
+        </Link>
+        <Link href={"#contact"}>
+          <li>
+              Contact
+          </li>
+        </Link>
       </ul>
     </div>
   )
