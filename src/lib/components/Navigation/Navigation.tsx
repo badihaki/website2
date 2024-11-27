a6b59d5c3ef6ddaf7c074c648499ae8a58a3a123
B. Haki Martin <jojackblack@gmail.com>
2024-11-26 23:52:25 -0500

    nav bar styling

--- src/lib/components/Navigation/Navigation.tsx
+++ src/lib/components/Navigation/Navigation.tsx
@@ -36,33 +36,21 @@ export default Navigation
 */
 "use client"
 
-import React, { useState } from 'react';
-import cx from 'clsx';
+import React from 'react';
 import {
-  Burger,
   Container,
-  Group,
-  Menu,
   Tabs,
-  UnstyledButton,
 } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
 import classes from './NavTabs.module.css';
 
 const tabs = [
-  'Home',
-  'Orders',
-  'Education',
-  'Community',
-  'Forums',
-  'Support',
-  'Account',
-  'Helpdesk',
+  'Welcome',
+  'WebDev',
+  'GameDev',
+  'Contact Me'
 ];
 
 export default function Navigation() {
-  const [opened, { toggle }] = useDisclosure(false);
-  const [userMenuOpened, setUserMenuOpened] = useState(false);
 
   const items = tabs.map((tab) => (
     <Tabs.Tab value={tab} key={tab}>
@@ -71,31 +59,9 @@ export default function Navigation() {
   ));
 
   return (
-    <div id='navigation' className='sticky top-10 min-w-full z-50 overscroll-none'>
+    <div id='navigation' className='sticky top-10 min-w-[90%] z-50 overscroll-none'>
 
       <div className={classes.header}>
-        <Container className={classes.mainSection} size="md">
-          <Group justify="space-between">
-
-            <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
-
-            <Menu
-              width={260}
-              position="bottom-end"
-              transitionProps={{ transition: 'pop-top-right' }}
-              onClose={() => setUserMenuOpened(false)}
-              onOpen={() => setUserMenuOpened(true)}
-              withinPortal
-            >
-              <Menu.Target>
-                <UnstyledButton
-                  className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
-                >
-                </UnstyledButton>
-              </Menu.Target>
-            </Menu>
-          </Group>
-        </Container>
         <Container size="md">
           <Tabs
             defaultValue="Home"
